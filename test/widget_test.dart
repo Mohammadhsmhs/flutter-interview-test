c58ba5879851a3c5b9b8fe75713742dc2ab12db4
mohammad <mohammad.hs.mhs@live.com>
2021-10-01 18:16:44 +0330

    UI test passed

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,25 +5,35 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_interview_test/presentation/screens/ToDo/todo.dart';
-import 'package:flutter_interview_test/presentation/widgets/todo/todo_item.dart';
+import 'package:flutter_interview_test/main.dart' as app;
+import 'package:integration_test/integration_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_interview_test/main.dart';
-
 void main() {
-  testWidgets('remove todo from list test', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
-    await tester.pumpWidget(Todo());
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  testWidgets("failing test example", (WidgetTester tester) async {
+    final BuildContext context = tester.element(find.byType(Container));
+    app.main();
+    await tester.pumpAndSettle();
 
-    await tester.pumpWidget(TodoItem(title: 'testing', index: 0));
+    expect(find.text("Oops! nothing to show :("), findsOneWidget);
+    final Finder fab = find.byIcon(Icons.add);
 
-    const title = 'testing';
-    expect(title, findsOneWidget);
+    await tester.tap(fab);
+    await tester.pumpAndSettle();
 
-    await tester.tap(find.byIcon(Icons.delete));
+    await tester.enterText(find.byType(TextField), 'hi');
 
-    expect(title, findsNothing);
+    await tester.tap(find.text("Save"));
+    await tester.pumpAndSettle();
+
+    expect(find.text('hi'), findsOneWidget);
+
+    final Finder del = find.byIcon(Icons.delete);
+    await tester.tap(del);
+    await tester.pumpAndSettle();
+    expect(find.text("Oops! nothing to show :("), findsOneWidget);
   });
 }
